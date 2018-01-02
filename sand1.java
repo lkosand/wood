9a3e92715d6295e34f992f80e583583f347bd7e3
lkosand <35016306+lkosand@users.noreply.github.com>
2018-01-02 16:25:31 +0800

    sand1
    输入数字，并从一输出到该数字

---
+++ sand1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class sand1{
+    
+	  public static void main(String[] args){
+//		Ctrl+shift+o 导包快捷键
+//		代码向上向下复制Ctrl+Alt+方向键上下
+		Scanner sc=new Scanner(System.in);
+		System.out.println("请输入一个数");
+		int b=sc.nextInt();
+		int[]arr;
+		arr=new int[b];
+		
+		for (int i=0;i<arr.length;i++){
+			System.out.println(i+1);
+			
+		}
+	}
+	}
