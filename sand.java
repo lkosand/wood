8c9005141a43e7f40dc7457b64fc18e6ca6188a9
lkosand <35016306+lkosand@users.noreply.github.com>
2018-01-02 16:26:23 +0800

    sand
    实践课2

---
+++ sand.java
@@ -0,0 +1,18 @@
+public class sand{
+	
+	public static void main(String[] args){
+		//TODO Auto-generated method stub
+		System.out.println("Äã»¹ºÃÂğ");
+		int a;
+		int b;
+		int result=add(10,15);
+		System.out.println(result);
+	}
+	public static int add(int c,int d){
+		return c+d;
+
+	}
+	
+	
+	
+}
